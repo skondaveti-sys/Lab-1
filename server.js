4080195ab4792b86e50c5fe85325b410b806ca1e
Shritan Kondaveti <shritan2014@gmail.com>
2026-09-18 17:13:21 +0000

    add About page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.set("view engine", "ejs");
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -8,6 +9,9 @@ app.get('/', (req, res) => {
 });
 app.get('/about', (req, res) => {
   res.send('This is a web programming course.');
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 app.listen(PORT, () => {
